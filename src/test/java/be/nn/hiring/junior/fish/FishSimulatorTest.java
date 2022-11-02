9634806cc74e71fca39805002689e537b0a84178
Cas Eliëns <cas.eliens@iodigital.com>
2022-11-02 12:57:59 +0100

    Change fish test format

--- src/test/java/be/nn/hiring/junior/fish/FishSimulatorTest.java
+++ src/test/java/be/nn/hiring/junior/fish/FishSimulatorTest.java
@@ -1,15 +1,13 @@
 package be.nn.hiring.junior.fish;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
-import be.nn.hiring.junior.fish.FishSimulator;
-
 class FishSimulatorTest {
     @ParameterizedTest
-    @CsvFileSource(resources = "/fishTestData.csv", numLinesToSkip = 1)
+    @CsvFileSource(resources = "/fishTestData.csv", numLinesToSkip = 1, delimiterString = ";")
     void testSimulators(int days, long expected, String input) {
 
         FishSimulator simulator = new FishSimulator();
